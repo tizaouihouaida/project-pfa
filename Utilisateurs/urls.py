0f611c8094b640b6e15ef432c549d9ca9e6e4765
GoldenDev74 <sessoukodjogoloden@gmail.com>
2025-01-06 04:14:36 +0000

    fornisseur

--- Utilisateurs/urls.py
+++ Utilisateurs/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.registration_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),  
     path('profil/', views.profile, name='profil'),
+    path('profil_vente/', views.profile_vente, name='profil_vente'),
     path('delete_account/', views.delete_account, name='delete_account'),
     path('change_password/', views.change_password, name='change_password'),
     path('change_password_ventes/', views.change_password_ventes, name='change_password_ventes'),
-    path('profil_vente/', views.profile_vente, name='profil_vente'),
-    path('login/', views.login_view, name='login'),
-    path('register/', views.registration_view, name='register'),
-    path('', views.home_view, name='home'),
-    path('logout/', views.logout_view, name='logout'),
 ]
